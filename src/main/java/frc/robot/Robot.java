7fb498432d8ffb61fba2252d6351c81e32accc33
erikD04 <erikdhalla04@gmail.com>
2023-01-17 16:16:59 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,27 +3,31 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardContainer;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.GenericEntry;
 
 public class Robot extends TimedRobot {
   private final WPI_VictorSPX m_rightMotor = new WPI_VictorSPX(0); // Note - Creates a new motor, because the motor exists physically not digitally yet.
   private final WPI_VictorSPX m_leftMotor = new WPI_VictorSPX(1); // Note - Creates another new motor
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor); // Note - Creates an instance of differential drive that takes in inputs of the left and right motors. */
   private final XboxController m_driverController = new XboxController(0); // Note - Creates an Xbox Controller object/instance
-  ShuffleboardTab joystickValues = Shuffleboard.getTab("Joystick Values");
+  private ShuffleboardTab joystickValues = Shuffleboard.getTab("Joystick Values");
+  /* private GenericEntry leftYValue = joystickValues.add("Left Y Value", 0)
+  .getEntry(); Note - Creates an entry variable that both creates values and sends them, and receives their value updates. Use only if putNumber doesn't work.*/
 
   @Override
   public void robotInit() {
     m_rightMotor.setInverted(true); // Note - Inverts the motors on one side because of the orientation physically. If not inverted, then it would turn.
-    CameraServer.startAutomaticCapture();        
+    CameraServer.startAutomaticCapture();
+    Shuffleboard.selectTab("Joystick Values"); 
+    Shuffleboard.getTab("Joystick Values")
+    .add("Joystick X Value", 0);       
   }
 
   @Override
@@ -34,6 +38,12 @@ public class Robot extends TimedRobot {
     act as 100%. This function, which is built into the library, has shortened this down for us so we only have to call one function. Because we gave the variable m_robotDrive the motor inputs, we just call the function without having to reference the motors. */
     m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX()); // Note - Takes in the controller's left Y axis & right x axis values to move the robot. LeftY moves back & forth, RightX turns.
     SmartDashboard.putNumber("Joystick X value" , m_driverController.getLeftY());
+    /* if (m_driverController.getLeftY() > 0 && m_driverController.getLeftY() < 0) {
+      Shuffleboard.getTab("Joystick Values")
+      .add("Left Y Value", m_driverController.getLeftY());
+    } Note - This line is meant to check if the y stick is above or below 0, or if it's moving up or down. If so, then it will access the Joystick Values tab and update the y position
+    with the current y axis calue with the .getLeftY method. It's placed here in case the line above this one doesn't update. */
+    // double yPosition = leftYValue.getDouble(1.0); Note - Playing around generic entries, use only if putNumber doesn't work as planned.
   }
 
   @Override
