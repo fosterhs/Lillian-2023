0a6eff26f279b89f115fef2293260c20def0a83f
kpratap1 <121640740+kpratap1@users.noreply.github.com>
2023-01-28 12:07:07 -0800

    Works with the new version of the 2022 robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,52 +1,78 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardComponent;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardContainer;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class Robot extends TimedRobot {
-  private final WPI_VictorSPX m_rightMotor = new WPI_VictorSPX(0); // Note - Creates a new motor, because the motor exists physically not digitally yet.
-  private final WPI_VictorSPX m_leftMotor = new WPI_VictorSPX(1); // Note - Creates another new motor
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor); // Note - Creates an instance of differential drive that takes in inputs of the left and right motors. */
-  private final XboxController m_driverController = new XboxController(0); // Note - Creates an Xbox Controller object/instance
-  ShuffleboardTab joystickValues = Shuffleboard.getTab("Joystick Values");
+  private final WPI_TalonFX left = new WPI_TalonFX(2); // left drive motor
+  private final WPI_TalonFX right = new WPI_TalonFX(0); // right drive motor
+  private final WPI_TalonFX belt = new WPI_TalonFX(1); // belt motor
+  private final WPI_TalonFX intakeInternal = new WPI_TalonFX(3); // internal intake motor
+  private final WPI_TalonFX intakeExternal = new WPI_TalonFX(4); // external intake motor
+  private final DifferentialDrive drive = new DifferentialDrive(left, right);
+  private final XboxController controller = new XboxController(0);
+  private final Timer timer = new Timer(); 
+  private final ADIS16448_IMU gyro = new ADIS16448_IMU(); // RoboRIO-mounted gyroscope
+  // controller inputs
+  double leftStickY;
+  double leftStickX;
+  double rightStickY;
+  double rightStickX;
+  double leftTrigger;
+  double rightTrigger;
+  // motor encoder values
+  double positionLeft;
+  double positionRight;
+  double positionBelt;
+  double positionInternalIntake;
+  double positionExternalIntake;
+  // match time
+  double time;
+  // gyro angle
+  double angle;
 
   @Override
   public void robotInit() {
-    m_rightMotor.setInverted(true); // Note - Inverts the motors on one side because of the orientation physically. If not inverted, then it would turn.
-    CameraServer.startAutomaticCapture();        
+    initialize();
   }
 
   @Override
-  public void robotPeriodic() {
-    /* Note - Arcade Drive function that takes in the controllers left Y axis values and right X values to move the robot. The controllers sticks will return a value between 1 
-    and -1, which when inputted into the function, will give the motors speed/movement. Motor movement is simple. It's 0% power at default, meaning it has 0% speed, but if you
-    give the motor 20% power, it has 20% speed. Similarlly if you take the 1 and -1 values the sticks give off and assign them to the motors, you have motor speed. Although 1 wouuld
-    act as 100%. This function, which is built into the library, has shortened this down for us so we only have to call one function. Because we gave the variable m_robotDrive the motor inputs, we just call the function without having to reference the motors. */
-    m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX()); // Note - Takes in the controller's left Y axis & right x axis values to move the robot. LeftY moves back & forth, RightX turns.
-    SmartDashboard.putNumber("Joystick X value" , m_driverController.getLeftY());
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {
+    timer.reset();
   }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousPeriodic() {
+    updateVariables();
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void teleopInit() {
+    timer.reset();
+  }
 
   @Override
-  public void teleopInit() {}
-
-  @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    updateVariables();
+    
+    // sets motor speeds based on controller inputs
+    drive.arcadeDrive(-leftStickY, -leftStickX, true);
+    belt.set(-rightTrigger);
+    intakeExternal.set(leftTrigger);
+    intakeInternal.set(leftTrigger);
+  }
 
   @Override
   public void disabledInit() {}
@@ -65,4 +91,113 @@ public class Robot extends TimedRobot {
 
   @Override
   public void simulationPeriodic() {}
+
+  public void initialize() {
+    motorStartUp(left);
+    //PID coefficients
+    left.config_kF(0, 0, 30);
+    left.config_kP(0, 1, 30);
+    left.config_kI(0, 0.005, 30);
+    left.config_kD(0, 10, 30);
+    // motion magic parameters
+    left.configMotionCruiseVelocity(20000, 30);
+    left.configMotionAcceleration(6000, 30);
+    // brake vs coast
+    left.setNeutralMode(NeutralMode.Brake);
+
+    motorStartUp(right);
+    right.config_kF(0, 0, 30);
+    right.config_kP(0, 1, 30);
+    right.config_kI(0, 0.005, 30);
+    right.config_kD(0, 10, 30);
+    right.configMotionCruiseVelocity(20000, 30);
+    right.configMotionAcceleration(6000, 30);
+    right.setNeutralMode(NeutralMode.Brake);
+    right.setInverted(true);
+
+    motorStartUp(belt);
+    belt.config_kF(0, 0, 30); 
+    belt.config_kP(0, 1, 30);
+    belt.config_kI(0, 0.005, 30);
+    belt.config_kD(0, 10, 30);
+    belt.configMotionCruiseVelocity(20000, 30);
+    belt.configMotionAcceleration(6000, 30);
+    belt.setNeutralMode(NeutralMode.Brake); 
+
+    motorStartUp(intakeInternal);
+    intakeInternal.config_kF(0, 0, 30);
+    intakeInternal.config_kP(0, 1, 30);
+    intakeInternal.config_kI(0, 0.005, 30);
+    intakeInternal.config_kD(0, 10, 30);
+    intakeInternal.configMotionCruiseVelocity(20000, 30);
+    intakeInternal.configMotionAcceleration(6000, 30);
+    intakeInternal.setNeutralMode(NeutralMode.Brake);
+
+    motorStartUp(intakeExternal);
+    intakeExternal.config_kF(0, 0, 30);
+    intakeExternal.config_kP(0, 1, 30);
+    intakeExternal.config_kI(0, 0.005, 30);
+    intakeExternal.config_kD(0, 10, 30);
+    intakeExternal.configMotionCruiseVelocity(20000, 30);
+    intakeExternal.configMotionAcceleration(6000, 30);
+    intakeExternal.setNeutralMode(NeutralMode.Brake);
+
+    timer.start();
+    timer.reset(); // sets the timer to 0
+    gyro.calibrate(); // sets the gyro angle to 0 based on the current robot position
+    CameraServer.startAutomaticCapture(); // starts the webcam stream
+
+    updateVariables();
+  }
+
+  // runs manufacturer recommended startup commands for Falcon 500 motors. Should be run at startup for all motors.
+  public void motorStartUp(WPI_TalonFX motor) {
+    motor.configFactoryDefault();
+    motor.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0 , 30);
+    motor.configNeutralDeadband(0.01, 30);
+    motor.setSensorPhase(false);
+    motor.setInverted(false);
+    motor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, 30);
+    motor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, 30);
+    motor.configNominalOutputForward(0, 30);
+    motor.configNominalOutputReverse(0, 30);
+    motor.configPeakOutputForward(1, 30);
+    motor.configPeakOutputReverse(-1, 30);
+    motor.selectProfileSlot(0, 0);
+    motor.configOpenloopRamp(1);
+    motor.setSelectedSensorPosition(0, 0, 30);
+  }
+  
+  // initializes variables and publishes values on dashboard
+  public void updateVariables() {
+    // updates variables
+    leftStickY = controller.getLeftY();
+    leftStickX = controller.getLeftX();
+    rightStickY = controller.getRightY();
+    rightStickX = controller.getRightX();
+    leftTrigger = controller.getLeftTriggerAxis();
+    rightTrigger = controller.getRightTriggerAxis();
+    positionLeft = left.getSelectedSensorPosition(0);
+    positionRight = right.getSelectedSensorPosition(0);
+    positionBelt = belt.getSelectedSensorPosition(0);
+    positionInternalIntake = intakeInternal.getSelectedSensorPosition(0);
+    positionExternalIntake = intakeExternal.getSelectedSensorPosition(0);
+    time = timer.get();
+    angle = gyro.getGyroAngleZ();
+    
+    // publishes updated variables to the dashboard
+    SmartDashboard.putNumber("leftStickY", leftStickY);
+    SmartDashboard.putNumber("leftStickX", leftStickX);
+    SmartDashboard.putNumber("rightStickY", rightStickY);
+    SmartDashboard.putNumber("rightStickX", rightStickX);
+    SmartDashboard.putNumber("rightTrigger", rightTrigger);
+    SmartDashboard.putNumber("leftTrigger", leftTrigger);
+    SmartDashboard.putNumber("Encoder (Left)", positionLeft);
+    SmartDashboard.putNumber("Encoder (Right)", positionRight);
+    SmartDashboard.putNumber("Encoder (Belt)", positionBelt);
+    SmartDashboard.putNumber("Encoder (External Intake)", positionExternalIntake);
+    SmartDashboard.putNumber("Encoder (Internal Intake)", positionInternalIntake);
+    SmartDashboard.putNumber("Clock",  time);
+    SmartDashboard.putNumber("Angle", angle);
+  }
 }
